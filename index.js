54eb20d7ebedda6e9c05853427aa9a0debe7361d
M Bussonnier <bussonniermatthias@gmail.com>
2024-06-06 10:56:35 -0700

    partial

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import { Router } from 'itty-router';
 
+const PYPI_URL = "https://pypi.org/simple/";
+const NIGHTLY_MIRROR = [
+    {
+        mirror: "https://pypi.anaconda.org/scientific-python-nightly-wheels/simple/",
+        root: "https://pypi.anaconda.org"
+    }
+];
 // Create a new router
 const router = Router();
 
@@ -16,53 +23,76 @@ URL.
 
 Try visit /example/hello and see the response.
 */
-router.get('/example/:text', ({ params }) => {
+router.get('/:package', async ({ params }) => {
 	// Decode text like "Hello%20world" into "Hello world"
-	let input = decodeURIComponent(params.text);
+	let input = decodeURIComponent(params.package);
 
-	// Serialise the input into a base64 string
-	let base64 = btoa(input);
+	return await handleSimple(params.package);
 
-	// Return the HTML with the string to the client
-	return new Response(`<p>Base64 encoding: <code>${base64}</code></p>`, {
-		headers: {
-			'Content-Type': 'text/html',
-		},
-	});
+	const response = await fetch(PYPI_URL+input);
+  const body = await response.text();
+  return new Response(body, {
+      headers: { 'Content-Type': 'text/html' }
+  });
+
 });
 
-/*
-This shows a different HTTP method, a POST.
 
-Try send a POST request using curl or another tool.
+async function handleSimple(packageName) {
+    let fixedNightly = [];
+		if (packageName.endsWith('.ico') ){
+			return
+		}
+    for (const { mirror, root } of NIGHTLY_MIRROR) {
+				const url = `${mirror}${packageName}`
+			  console.log('fetching', url)
 
-Try the below curl command to send JSON:
+        const mirrorResponse = await fetch(url);
+        const mirrorBody = await mirrorResponse.text();
 
-$ curl -X POST <worker> -H "Content-Type: application/json" -d '{"abc": "def"}'
-*/
-router.post('/post', async request => {
-	// Create a base object with some fields.
-	let fields = {
-		asn: request.cf.asn,
-		colo: request.cf.colo,
-	};
+        const rewriter = new HTMLRewriter()
+            .on('a', {
+                element(element) {
+                    const href = element.getAttribute('href');
 
-	// If the POST data is JSON then attach it to our response.
-	if (request.headers.get('Content-Type') === 'application/json') {
-		let json = await request.json();
-		Object.assign(fields, { json });
-	}
+										console.log('element', element, href)
+                    if (href && href.startsWith('/')) {
+												const newHref = root + href;
+												let linkHtml = `<a href="${newHref}">`;
+                        element.setInnerContent(innerContent => {
+														console.log('inner contnetn', innerContent)
+                            linkHtml += innerContent +'Dev debug';
+                        });
+                        element.onEndTag(tag => {
+                            linkHtml += 'Dev debug</a>';
+                            fixedNightly.push(linkHtml);
+                        });
+                        element.setAttribute('href', root + href);
 
-	// Serialise the JSON to a string.
-	const returnData = JSON.stringify(fields, null, 2);
 
-	return new Response(returnData, {
-		headers: {
-			'Content-Type': 'application/json',
-		},
-	});
-});
+                        //fixedNightly.push(element);
+                        // Push the modified link HTML to fixedNightly array
 
+                    }
+                }
+            });
+        await rewriter.transform(new Response(mirrorBody)).text();
+    }
+
+		console.log('!!fixed nightly',fixedNightly);
+
+    const pypiResponse = await fetch(`${PYPI_URL}${packageName}`);
+    const pypiBody = await pypiResponse.text();
+    let combinedBody = pypiBody;
+
+    for (const nightlyElement of fixedNightly) {
+        combinedBody = combinedBody.replace('</body>', nightlyElement + '</body>');
+    }
+
+    return new Response(combinedBody, {
+        headers: { 'Content-Type': 'text/html' }
+    });
+}
 /*
 This is the last route we define, it will match anything that hasn't hit a route we've defined
 above, therefore it's useful as a 404 (and avoids us hitting worker exceptions, so make sure to include it!).
